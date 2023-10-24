1f73c0c17357d2487b5e19194f19fbce1da046df
arunbalaram89 <53626186+arunbalaram89@users.noreply.github.com>
2023-10-23 23:39:15 -0400

    Add files via upload

--- PyBank/budget.py
+++ PyBank/budget.py
@@ -2,14 +2,15 @@ import os
 import csv
 import math
 
-budgetpath = os.path.join('..','Resources','budget_data.csv')
+budgetpath = os.path.join('Resources','budget_data.csv')
 
 
 ttl_months = []
 net_profit = []
 avg_change = []
-greatest_increase_profits = []
-greatest_decrease_profits = []
+greatest_increase_profits = [-100000000, ""]
+greatest_decrease_profits = [100000000, ""]
+prev_net = 0
 
 # Method 1: Plain Read of Budget Data CSV file
 
@@ -25,55 +26,66 @@ with open(budgetpath, 'r') as budget_handler:
 
 with open(budgetpath, 'r') as csvfile:
     budgetreader = csv.reader(csvfile, delimiter=',')
-   
-    for column in budgetreader:
-        header = next(budgetreader)
+    header = next(budgetreader)
+
+    for i,column in enumerate (budgetreader):
         # Add Total Months
         ttl_months.append(column[0])
         # Add Net Profit
-        net_profit.append(column[1])
+        net_profit.append(int(column[1]))
+        if i > 0:
         # Get the average change 
         # Determine changes of Profit/Loss and then average of changes
-        prev_net = int(first_row[1])
-        net_change = int(first_row[1]) - prev_net
-        net_change_list -= net_change
-        avg_change = sum(net_change_list) / len(net_change_list)
-        avg_change.append(column[1])
-    
+        
+            
+            net_change = int(column[1]) - prev_net
+            avg_change.append(net_change)
+            if net_change > greatest_increase_profits[0]:
+                greatest_increase_profits[0] = net_change
+                greatest_increase_profits[1] = column[0]
+
+            if net_change < greatest_decrease_profits[0]:
+                greatest_decrease_profits[0] = net_change
+                greatest_decrease_profits[1] = column[0]
+        prev_net = int(column[1])       
+
         # Determine greatest increase in Profits (Date and Amount) over the entire period
-        High_profit += greatest_increase_profits
-        greatest_increase_profits.append(int+=(column[1]))
-        # Determine greatest decrease in profits (Date and Amount) over the entire period
-        Lowest_profit -= greatest_decrease_profits
-        greatest_decrease_profits.append(int-=(column[1]))
+        # High_profit += greatest_increase_profits
+        # greatest_increase_profits.append(int+=(column[1]))
+        # # Determine greatest decrease in profits (Date and Amount) over the entire period
+        # Lowest_profit -= greatest_decrease_profits
+        # greatest_decrease_profits.append(int-=(column[1]))
 
 
 
 
 # Zip file in cleaned csv 
-cleaned_budget_csv = zip(ttl_months, net_profit, avg_change, greatest_increase_profits, greatest_decrease_profits)
+# cleaned_budget_csv = zip(ttl_months, net_profit, avg_change, greatest_increase_profits, greatest_decrease_profits)
 
 #Set variable for the output file
 budget_output_file = os.path.join("budget_final.txt")
+output = "BUDGET DATA\n"
+output+= "-------------------------------------\n"
+output+= f"Total number of months in the budget data is: {len(ttl_months)}\n"
+output+= f"The total net total amount of Profit and Losses over the entire period is ${sum(net_profit)}\n"
+output+= f"The changes in Profit/Losses over the entire period, and then the average of those changes ${round(sum(avg_change)/len(avg_change), 2)}\n"
+output+= f"The greatest increase in profits (date and amount) over the entire period {greatest_increase_profits[1]} ${greatest_increase_profits[0]}\n"
+output+= f"The greatest decrease in profits (date and amount) over the entire period {greatest_decrease_profits[1]} ${greatest_decrease_profits[0]}"
 
 
 #  Open the output file
 with open(budget_output_file, "w") as budget_datafile:
-    budgetwriter = csv.writer(budget_datafile)
+    # budgetwriter = csv.writer(budget_datafile)
+    budget_datafile.write(output)
 
     # Write the header row
-    budgetwriter.writerow(["Total Months", "Net Profits" , "Average Change", "Greatest Increase in Profit", "Greatest Decrease in Profit"])
+    # budgetwriter.writerow(["Total Months", "Net Profits" , "Average Change", "Greatest Increase in Profit", "Greatest Decrease in Profit"])
 
     # Write in zipped rows
-    budgetwriter.writerows(cleaned_budget_csv.txt)
+    # budgetwriter.writerows(cleaned_budget_csv.txt)
 
 
 
-print("BUDGET DATA")
-print("-------------------------------------")
-print(f"Total number of months in the budget data is: {ttl_months}")
-print(f"The total net total amount of Profit and Losses over the entire period is{net_profit}")
-print(f"The changes in Profit/Losses over the entire period, and then the average of those changes{avg_change}")
-print(f"The greatest increase in profits (date and amount) over the entire period{greatest_increase_profits}")
-print(f"The greatest decrease in profits (date and amount) over the entire period{greatest_decrease_profits}")
+print(output)
+
 
--- PyPoll/election.py
+++ PyPoll/election.py
@@ -2,19 +2,14 @@ import os
 import csv
 import math
 
-electionpath = os.path.join('..','Resources','election_data.csv')
+electionpath = os.path.join('Resources','election_data.csv')
 
-ttl_votes = []
-Charles = []
-Diana = []
-Raymon = []    
-Winner = []
+ttl_votes = 0
+candidates = {}
+
 
 # Election Data 
 
-with open(electionpath, 'r') as file_handler:
-    electionlines = file_handler.read()
-    print(electionlines)
 
 # Read the header row first
 
@@ -23,52 +18,41 @@ with open(electionpath, 'r') as csvfile:
     election_header = next(electionreader)
     print(f"Election Headers:{election_header}")
 
-   
-
-
     for polling in electionreader:
-        election_header = next(electionreader)
         # Total Election votes
-        ttl_votes.append(polling[0])
-        # Diana's Election result 
-        diana_voting = candidate1 == "Diana DeGette",
-        Diana.append(polling[2])
-        # Charles Casper Stockham
-        charles_voting = candidate2 == "Charles Casper Stockham"
-        Charles.append(polling[2])
-        # Raymon Anthony Doane
-        raymon_voting = candidate3 == "Raymon Anthony Doane"
-        Raymon.append(polling[2])
-        #Winner 
-        winner_voting = electioncount
-        Winner.append(polling[2])
+        ttl_votes += 1
+        try: 
+            candidates[polling[2]]+=1
+        except:
+            candidates[polling[2]]=1    
 
 
 # Zip file in cleaned csv 
-cleaned_election_csv = zip(ttl_votes, Charles, Diana, Raymon, Winner)
 
 #Set variable for the output file
 election_output_file = os.path.join("election_final.txt")
+output = "Election Results\n"
+output+= "------------------------------------\n"
+output+= f"Total Votes: {ttl_votes}\n"
+output+= "-------------------------------------\n"
+winner = [0, ""]
+for name, votes in candidates.items():
+    output+=f"{name}: {round(votes/ttl_votes * 100, 2)}% ({votes})\n"
+    if votes > winner[0]:
+        winner[0] = votes
+        winner[1] = name
+output+= "-------------------------------------\n"
+output+= f"Winner: {winner[1]}"
+
+
 
 
 #  Open the output file
 with open(election_output_file, "w") as election_datafile:
-    election_write = csv.writer(election_datafile)
-
-    # Write the header row
-    election_write.writerow(["Election Results"])
-
-    # Write in zipped rows
-    election_write.writerows(election_datafile.txt)
+   election_datafile.write(output)
 
 
 
-print("Election Results")
-print ("------------------------------------")
-print("Total Votes: {ttl_votes}")
-print("-------------------------------------")
-print("Charles Casper Stockham: {Charles}")
-print("Diana DeGette {Diana}")
-print("Raymon Anthony Doane {Raymon}")
-print("-------------------------------------")
-print("Winner: {Winner}")
+print(output)
+
+
